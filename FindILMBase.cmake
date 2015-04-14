411ae1d742284b403184778f70307d9f98a736ab
Jason Marshall <jmarshal@caltech.edu>
2015-04-14 13:14:32 -0700

    Update ILMBase find.

--- FindILMBase.cmake
+++ FindILMBase.cmake
@@ -6,6 +6,29 @@
 #  ILMBase_LIBRARIES - link these to use ILMBase
 # questions: jason.p.marshall at gmail dot com
 #
+
+find_path(ILMBase_INCLUDE_DIR OpenEXR)
+
+find_library(ILMBaseHalf_LIBRARY NAMES Half)
+find_library(ILMBaseIex_LIBRARY NAMES Iex)
+find_library(ILMBaseIexMath_LIBRARY NAMES IexMath)
+find_library(ILMBaseIlmImf_LIBRARY NAMES IlmImf)
+find_library(ILMBaseIlmImfUtil_LIBRARY NAMES IlmImfUtil)
+find_library(ILMBaseIlmThread_LIBRARY NAMES IlmThread)
+find_library(ILMBaseImath_LIBRARY NAMES Imath)
+
+# handle the QUIETLY and REQUIRED arguments and set ILMBase_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ILMBase DEFAULT_MSG ILMBaseHalf_LIBRARY ILMBaseIex_LIBRARY ILMBaseIexMath_LIBRARY ILMBaseIlmImf_LIBRARY ILMBaseIlmImfUtil_LIBRARY ILMBaseIlmThread_LIBRARY ILMBaseImath_LIBRARY ILMBase_INCLUDE_DIR)
+
+if(ILMBase_FOUND)
+  set(ILMBase_LIBRARIES ${ILMBaseHalf_LIBRARY} ${ILMBaseIex_LIBRARY} ${ILMBaseIexMath_LIBRARY} ${ILMBaseIlmImf_LIBRARY} ${ILMBaseIlmImfUtil_LIBRARY} ${ILMBaseIlmThread_LIBRARY} ${ILMBaseImath_LIBRARY})
+endif()
+
+mark_as_advanced(ILMBase_INCLUDE_DIR ILMBaseHalf_LIBRARY ILMBaseIex_LIBRARY ILMBaseIexMath_LIBRARY ILMBaseIlmImf_LIBRARY ILMBaseIlmImfUtil_LIBRARY ILMBaseIlmThread_LIBRARY ILMBaseImath_LIBRARY ILMBase_LIBRARIES)
+
+if(0)
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
@@ -60,3 +83,4 @@ find_library(ILMBaseImath_LIBRARY
 set(ILMBase_PROCESS_INCLUDES ILMBase_INCLUDE_DIR)
 set(ILMBase_PROCESS_LIBS ILMBaseHalf_LIBRARY ILMBaseIex_LIBRARY ILMBaseIexMath_LIBRARY ILMBaseIlmImf_LIBRARY ILMBaseIlmImfUtil_LIBRARY ILMBaseIlmThread_LIBRARY ILMBaseImath_LIBRARY)
 libfind_process(ILMBase)
+endif()

84a389456e8412767ce80df4280394464358253a
Jason Marshall <jmarshal@caltech.edu>
2015-04-14 16:50:02 -0700

    Clean up stlib find file.

--- FindSTLIB.cmake
+++ FindSTLIB.cmake
@@ -7,17 +7,11 @@
 # questions: jason.p.marshall at gmail dot com
 #
 
-find_path(STLIB_INCLUDE_DIR blosc.h)
-
-find_library(STLIB_LIBRARY NAMES blosc)
+find_path(STLIB_INCLUDE_DIR stlib)
 
 # handle the QUIETLY and REQUIRED arguments and set STLIB_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(STLIB DEFAULT_MSG STLIB_LIBRARY STLIB_INCLUDE_DIR)
+find_package_handle_standard_args(STLIB DEFAULT_MSG STLIB_INCLUDE_DIR)
 
-if(STLIB_FOUND)
-  set(STLIB_LIBRARIES ${STLIB_LIBRARY})
-endif()
-
-mark_as_advanced(STLIB_INCLUDE_DIR STLIB_LIBRARY STLIB_LIBRARIES)
+mark_as_advanced(STLIB_INCLUDE_DIR)

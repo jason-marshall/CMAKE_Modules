0efb0fe93698df733060b9c2eb6ea81666353363
Jason Marshall <jmarshal@caltech.edu>
2015-04-14 13:34:55 -0700

    Update tbb find file.

--- FindOpenEXR.cmake
+++ FindOpenEXR.cmake
@@ -14,23 +14,3 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(OpenEXR DEFAULT_MSG OpenEXR_INCLUDE_DIR)
 
 mark_as_advanced(OpenEXR_INCLUDE_DIR)
-
-if(0)
-include(LibFindMacros)
-
-# Use pkg-config to get hints about paths
-libfind_pkg_check_modules(OpenEXR_PKGCONF OpenEXR)
-
-# Include dir
-find_path(OpenEXR_INCLUDE_DIR
-  NAMES OpenEXR
-  PATHS ${OpenEXR_PKGCONF_INCLUDE_DIRS}
-  )
-
-set(OpenEXR_INCLUDE_DIR ${OpenEXR_INCLUDE_DIR}/OpenEXR)
-
-# Set the include dir variables and the libraries
-# NOTE: Singular variables for this library, plural for libraries this lib depends on.
-set(OpenEXR_PROCESS_INCLUDES OpenEXR_INCLUDE_DIR)
-libfind_process(OpenEXR)
-endif()
--- FindTBB.cmake
+++ FindTBB.cmake
@@ -6,6 +6,25 @@
 #  TBB_LIBRARIES - link these to use TBB
 # questions: jason.p.marshall at gmail dot com
 #
+
+find_path(TBB_INCLUDE_DIR tbb)
+
+find_library(TBB_LIBRARY NAMES tbb)
+find_library(TBBMALLOC_LIBRARY NAMES tbbmalloc)
+find_library(TBBMALLOCPROXY_LIBRARY NAMES tbbmalloc_proxy)
+
+# handle the QUIETLY and REQUIRED arguments and set TBB_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(TBB DEFAULT_MSG TBB_LIBRARY TBBMALLOC_LIBRARY TBBMALLOCPROXY_LIBRARY TBB_INCLUDE_DIR)
+
+if(TBB_FOUND)
+  set(TBB_LIBRARIES ${TBB_LIBRARY} ${TBBMALLOC_LIBRARY} ${TBBMALLOCPROXY_LIBRARY})
+endif()
+
+mark_as_advanced(TBB_INCLUDE_DIR TBB_LIBRARY TBB_LIBRARIES)
+
+if(0)
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
@@ -39,3 +58,4 @@ find_library(TBBMALLOCPROXY_LIBRARY
 set(TBB_PROCESS_INCLUDES TBB_INCLUDE_DIR)
 set(TBB_PROCESS_LIBS TBB_LIBRARY TBBMALLOC_LIBRARY TBBMALLOCPROXY_LIBRARY)
 libfind_process(TBB)
+endif()

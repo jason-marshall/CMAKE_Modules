20de440e88929914ec5613d6c3f51ceed6082ff1
Jason Marshall <jmarshal@caltech.edu>
2015-04-14 15:30:42 -0700

    Add external cdemlab cmake file.

---
+++ External_CDEMLAB.cmake
@@ -0,0 +1,24 @@
+# An external project for cdemlab
+set(cdemlab_prefix "${CMAKE_CURRENT_BINARY_DIR}/modules/example")
+
+# switch between operating systems
+if(WIN32)
+
+else()
+  ExternalProject_Add(cdemlab
+    DEPENDS ${CDEMLAB_DEPENDENCIES}
+    # specify location
+    PREFIX ${cdemlab_prefix}
+    # src location
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src
+    # install location
+    INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    )
+
+  #ExternalProject_Add_Step(example forcebuild
+  #  COMMAND ${CMAKE_COMMAND} -E echo "Force build of example"
+  #  DEPENDERS configure
+  #  ALWAYS 1
+  #)
+endif()

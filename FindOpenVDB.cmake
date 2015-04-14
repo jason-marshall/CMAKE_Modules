f878d6b894151739f2229dd765b7ab00e3c5cb79
Jason Marshall <jmarshal@caltech.edu>
2015-04-14 15:15:02 -0700

    Update openvdb find file.

--- FindOpenVDB.cmake
+++ FindOpenVDB.cmake
@@ -6,6 +6,23 @@
 #  OpenVDB_LIBRARIES - link these to use OpenVDB
 # questions: jason.p.marshall at gmail dot com
 #
+
+find_path(OpenVDB_INCLUDE_DIR openvdb)
+
+find_library(OpenVDB_LIBRARY NAMES openvdb)
+
+# handle the QUIETLY and REQUIRED arguments and set OpenVDB_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OpenVDB DEFAULT_MSG OpenVDB_LIBRARY OpenVDB_INCLUDE_DIR)
+
+if(OpenVDB_FOUND)
+  set(OpenVDB_LIBRARIES ${OpenVDB_LIBRARY})
+endif()
+
+mark_as_advanced(OpenVDB_INCLUDE_DIR OpenVDB_LIBRARY OpenVDB_LIBRARIES)
+
+if(0)
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
@@ -30,3 +47,4 @@ set(OpenVDB_INCLUDE_DIR ${OpenVDB_INCLUDE_DIR}/OpenVDB)
 set(OpenVDB_PROCESS_INCLUDES OpenVDB_INCLUDE_DIR )
 set(OpenVDB_PROCESS_LIBS OpenVDB_LIBRARY)
 libfind_process(OpenVDB)
+endif()
--- FindTBB.cmake
+++ FindTBB.cmake
@@ -23,39 +23,3 @@ if(TBB_FOUND)
 endif()
 
 mark_as_advanced(TBB_INCLUDE_DIR TBB_LIBRARY TBB_LIBRARIES)
-
-if(0)
-include(LibFindMacros)
-
-# Use pkg-config to get hints about paths
-libfind_pkg_check_modules(TBB_PKGCONF TBB)
-
-# Include dir
-find_path(TBB_INCLUDE_DIR
-  NAMES tbb
-  PATHS ${TBB_PKGCONF_INCLUDE_DIRS}
-  )
-
-set(TBB_INCLUDE_DIR ${TBB_INCLUDE_DIR}/tbb)
-
-find_library(TBB_LIBRARY
-  NAMES tbb
-  PATHS ${TBB_PKGCONF_LIBRARY_DIRS}
-  )
-
-find_library(TBBMALLOC_LIBRARY
-  NAMES tbbmalloc
-  PATHS ${TBB_PKGCONF_LIBRARY_DIRS}
-  )
-
-find_library(TBBMALLOCPROXY_LIBRARY
-  NAMES tbbmalloc_proxy
-  PATHS ${TBB_PKGCONF_LIBRARY_DIRS}
-  )
-
-# Set the include dir variables and the libraries
-# NOTE: Singular variables for this library, plural for libraries this lib depends on.
-set(TBB_PROCESS_INCLUDES TBB_INCLUDE_DIR)
-set(TBB_PROCESS_LIBS TBB_LIBRARY TBBMALLOC_LIBRARY TBBMALLOCPROXY_LIBRARY)
-libfind_process(TBB)
-endif()

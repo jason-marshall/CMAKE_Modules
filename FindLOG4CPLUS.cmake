c93217bc0cc43afd74fb014af57a639921229371
Jason Marshall <jmarshal@caltech.edu>
2015-04-14 13:26:59 -0700

    Update log4cplus find file.

--- FindLOG4CPLUS.cmake
+++ FindLOG4CPLUS.cmake
@@ -6,6 +6,23 @@
 #  LOG4CPLUS_LIBRARIES - link these to use LOG4CPLUS
 # questions: jason.p.marshall at gmail dot com
 #
+
+find_path(LOG4CPLUS_INCLUDE_DIR LOG4CPLUS)
+
+find_library(LOG4CPLUS_LIBRARY NAMES log4cplus)
+
+# handle the QUIETLY and REQUIRED arguments and set LOG4CPLUS_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LOG4CPLUS DEFAULT_MSG LOG4CPLUS_LIBRARY LOG4CPLUS_INCLUDE_DIR)
+
+if(LOG4CPLUS_FOUND)
+  set(LOG4CPLUS_LIBRARIES ${LOG4CPLUS_LIBRARY})
+endif()
+
+mark_as_advanced(LOG4CPLUS_INCLUDE_DIR LOG4CPLUS_LIBRARY LOG4CPLUS_LIBRARIES)
+
+if(0)
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
@@ -30,3 +47,4 @@ find_library(LOG4CPLUS_LIBRARY
 set(LOG4CPLUS_PROCESS_INCLUDES LOG4CPLUS_INCLUDE_DIR)
 set(LOG4CPLUS_PROCESS_LIBS LOG4CPLUS_LIBRARY)
 libfind_process(LOG4CPLUS)
+endif()

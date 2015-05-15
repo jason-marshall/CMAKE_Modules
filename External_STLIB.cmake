b930fc3a54527b1b86d03e70d9394764c4e4f191
Jason Marshall <jmarshal@caltech.edu>
2015-05-15 14:11:08 -0700

    Update stlib external make.

--- External_STLIB.cmake
+++ External_STLIB.cmake
@@ -15,6 +15,6 @@ else()
     INSTALL_DIR ${INSTALL_DIRECTORY}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
-    INSTALL_COMMAND cp -r ${stlib_prefix}/src/stlib/src ${CMAKE_INSTALL_PREFIX}/include/stlib
+    INSTALL_COMMAND cp -r ${stlib_prefix}/src/stlib/stlib ${CMAKE_INSTALL_PREFIX}/include/stlib
     )
 endif()

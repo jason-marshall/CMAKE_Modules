a6f7b0cf31a23b8735ce1610e8245882575fc723
Jason Marshall <jmarshal@caltech.edu>
2015-05-22 12:17:07 -0700

    Another update.

--- External_BOOST.cmake
+++ External_BOOST.cmake
@@ -1,6 +1,7 @@
 # An external project for boost
 set(boost_prefix "${CMAKE_CURRENT_BINARY_DIR}/modules/Boost")
 set(boost_file "http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.zip/download")
+set(boost_md5 b0605a9323f1e960f7434dbbd95a7a5c)
 
 if(WIN32)
   message("Need to add boost for windows build.")
@@ -10,10 +11,12 @@ else()
     PREFIX ${boost_prefix}
     # download stuff
     URL ${boost_file}
+    URL_MD5 ${boost_md5}
     # patch command fix
     PATCH_COMMAND ""
     # build in source
     BUILD_IN_SOURCE 1
+    BUILD_COMMAND ""
     # configure command
     CONFIGURE_COMMAND ""
     # install directory

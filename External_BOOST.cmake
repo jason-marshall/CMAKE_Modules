ffc74ab3efe339674bdb4b49b08f6c765530fbdc
Jason Marshall <jmarshal@caltech.edu>
2015-05-22 12:02:26 -0700

    Update slightly.

--- External_BOOST.cmake
+++ External_BOOST.cmake
@@ -1,11 +1,11 @@
 # An external project for boost
-set(boost_prefix "${CMAKE_CURRENT_BINARY_DIR}/modules/boost")
+set(boost_prefix "${CMAKE_CURRENT_BINARY_DIR}/modules/Boost")
 set(boost_file "http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.zip/download")
 
 if(WIN32)
   message("Need to add boost for windows build.")
 else()
-  ExternalProject_Add(boost
+  ExternalProject_Add(Boost
     # specify build location
     PREFIX ${boost_prefix}
     # download stuff

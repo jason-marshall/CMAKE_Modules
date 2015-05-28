fbaf26d492d57293e6b76657833c00a2729d73cc
Jason Marshall <jmarshal@caltech.edu>
2015-05-28 15:45:24 -0700

    Add find gtengine file.

---
+++ FindGTENGINE.cmake
@@ -0,0 +1,17 @@
+# - Try to find GTENGINE
+# Once done, this will define
+#
+#  GTENGINE_FOUND - system has GTENGINE
+#  GTENGINE_INCLUDE_DIR - the GTENGINE include directories
+#  GTENGINE_LIBRARIES - link these to use GTENGINE
+# questions: jason.p.marshall at gmail dot com
+#
+
+find_path(GTENGINE_INCLUDE_DIR gtengine)
+
+# handle the QUIETLY and REQUIRED arguments and set GTENGINE_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GTENGINE DEFAULT_MSG GTENGINE_INCLUDE_DIR)
+
+mark_as_advanced(GTENGINE_INCLUDE_DIR)

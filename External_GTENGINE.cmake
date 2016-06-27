f1b73bfd79c6acdbf5ff99d7a6c8ba422e33a92c
Jason Marshall <jmarshal@caltech.edu>
2016-06-27 14:07:26 -0700

    Update gtengine.

--- External_GTENGINE.cmake
+++ External_GTENGINE.cmake
@@ -14,7 +14,7 @@ else()
     # install location
     INSTALL_DIR ${INSTALL_DIRECTORY}
     BUILD_IN_SOURCE 1
-    BUILD_COMMAND cd ${gtengine_prefix}/src/gtengine/GTEngine && make CFG=ReleaseStatic -f makeengine.gte
+    BUILD_COMMAND cd ${gtengine_prefix}/src/gtengine/GTEngine && make CFG=Release -f makeengine.gte
     CONFIGURE_COMMAND ""
     INSTALL_COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/prefix/include/gtengine &&  sh -c "cp -r ${gtengine_prefix}/src/gtengine/GTEngine/Include/* ${CMAKE_CURRENT_BINARY_DIR}/prefix/include/gtengine/." && ln -sfv ${gtengine_prefix}/src/gtengine/GTEngine/lib/ReleaseStatic/libgtengine.a ${CMAKE_CURRENT_BINARY_DIR}/prefix/lib/libgtengine.so
     )

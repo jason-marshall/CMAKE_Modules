9dbcbb7fee4377843beca9e511ba6d4e41a66e7f
Jason Marshall <jmarshal@caltech.edu>
2016-07-07 09:57:01 -0700

    Update gtengine.

--- External_GTENGINE.cmake
+++ External_GTENGINE.cmake
@@ -1,6 +1,7 @@
 # An external project for gtengine
 set(gtengine_prefix "${CMAKE_CURRENT_BINARY_DIR}/modules/gtengine")
 set(gtengine_url "http://www.geometrictools.com/Downloads/GeometricToolsEngine3p2.zip")
+set(GTE_PATH "${gtengine_prefix}/src/gtengine/GTEngine")
 
 # If Windows we use CMake, otherwise ./configure
 if(WIN32)
@@ -14,7 +15,7 @@ else()
     # install location
     INSTALL_DIR ${INSTALL_DIRECTORY}
     BUILD_IN_SOURCE 1
-    BUILD_COMMAND cd ${gtengine_prefix}/src/gtengine/GTEngine && make CFG=Release -f makeengine.gte
+    BUILD_COMMAND cd ${GTE_PATH}  && make CFG=Release -f makeengine.gte
     CONFIGURE_COMMAND ""
     INSTALL_COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/prefix/include/gtengine &&  sh -c "cp -r ${gtengine_prefix}/src/gtengine/GTEngine/Include/* ${CMAKE_CURRENT_BINARY_DIR}/prefix/include/gtengine/." && cp ${gtengine_prefix}/src/gtengine/GTEngine/lib/Release/libgtengine.a ${CMAKE_CURRENT_BINARY_DIR}/prefix/lib/libgtengine.a
     )

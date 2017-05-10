1a1ed14dd7b02a13303d9ce3dcc97440b1bb8a71
Jason Marshall <jmarshal@caltech.edu>
2017-05-10 08:40:06 -0700

    Update zlib.

--- External_ZLIB.cmake
+++ External_ZLIB.cmake
@@ -4,7 +4,7 @@ set(zlib_source  "${CMAKE_CURRENT_BINARY_DIR}/modules/src/zlib")
 set(zlib_build   "${CMAKE_CURRENT_BINARY_DIR}/modules/build/zlib")
 set(zlib_prefix "${CMAKE_CURRENT_BINARY_DIR}/modules/zlib")
 set(zlib_md5 44d667c142d7cda120332623eab69f40)
-set(zlib_file "http://zlib.net/zlib-1.2.8.tar.gz")
+set(zlib_file "http://zlib.net/zlib-1.2.11.tar.gz")
 
 # If Windows we use CMake, otherwise ./configure
 # seriously doubt that zlib works on windows
@@ -18,21 +18,21 @@ if(WIN32)
     URL_MD5 ${zlib_md5}
     PATCH_COMMAND ${CMAKE_COMMAND} -E remove <SOURCE_DIR>/zconf.h
     CMAKE_CACHE_ARGS
-      -DCMAKE_CXX_FLAGS:STRING=${pv_tpl_cxx_flags}
-      -DCMAKE_C_FLAGS:STRING=${pv_tpl_c_flags}
-      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_CFG_INTDIR}
-      ${pv_tpl_compiler_args}
-      ${zlib_EXTRA_ARGS}
+    -DCMAKE_CXX_FLAGS:STRING=${pv_tpl_cxx_flags}
+    -DCMAKE_C_FLAGS:STRING=${pv_tpl_c_flags}
+    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_CFG_INTDIR}
+    ${pv_tpl_compiler_args}
+    ${zlib_EXTRA_ARGS}
     CMAKE_ARGS
-      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>)
+    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>)
 
   # the zlib library should be named zlib1.lib not zlib.lib
   ExternalProject_Add_Step(zlib RenameLib
     COMMAND ${CMAKE_COMMAND} -E copy "${zlib_install}/lib/zlib.lib"
-      "${zlib_install}/lib/zlib1.lib"
-      DEPENDEES install)
+    "${zlib_install}/lib/zlib1.lib"
+    DEPENDEES install)
 
-    #  set(ZLIB_LIBRARY "${zlib_install}/lib/zlib1.${link_library_suffix}")
+  #  set(ZLIB_LIBRARY "${zlib_install}/lib/zlib1.${link_library_suffix}")
 else()
   ExternalProject_Add(zlib
     # specify build location
